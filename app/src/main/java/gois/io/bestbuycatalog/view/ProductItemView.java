e248bc5276c0f5003b1138d7eb1197118774a77e
Guilherme <guijocargo@live.com>
2016-01-15 01:45:34 -0200

    Tela de catálogo criada, falta acertar os painéis para exibir os produtos e montar a tela de detalhes, tudo estaticamente para depois integrar com a API da BestBuy.com. Por enquanto apenas modo portrait no smartphone, depois de inegrar com a API será feito o modo landscape e ambos para tablet.

---
+++ app/src/main/java/gois/io/bestbuycatalog/view/ProductItemView.java
@@ -0,0 +1,85 @@
+package gois.io.bestbuycatalog.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import gois.io.bestbuycatalog.R;
+import gois.io.bestbuycatalog.model.Product;
+import gois.io.bestbuycatalog.task.DownloadImageTask;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class ProductItemView extends GridView {
+    private final String CATEG = "ProductItemView";
+
+    private Product product;
+
+    public ProductItemView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public ProductItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ProductItemView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        Log.i(CATEG, "Initializing...");
+
+        LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        setLayoutParams(layoutParams);
+
+        Log.i(CATEG, "Initialized...");
+    }
+
+    public View build(View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.product_item_view, parent, false);
+        }
+        ImageView image = (ImageView) convertView.findViewById(R.id.productImageView);
+        new DownloadImageTask(image).execute(product.getImageURLs("item"));
+
+        TextView name = (TextView) convertView.findViewById(R.id.nameTextView);
+        name.setText(product.getName());
+
+        TextView brand = (TextView) convertView.findViewById(R.id.brandTextView);
+        brand.setText(product.getBrand());
+
+        TextView price = (TextView) convertView.findViewById(R.id.priceTextView);
+        price.setText("$ " + product.getPrice());
+
+        return convertView;
+    }
+}
